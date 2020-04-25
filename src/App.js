7337eaad4bedfb9fee308ddb4fc9000a31d323c4
DevCarlosU <c.carlos.umana@gmail.com>
2020-04-25 17:55:33 -0500

    GlobalStyles created and added to App component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyles } from './styles/GlobalStyles'
 
 export const App = () => {
   return (
-    <div>
+    <ThemeProvider>
+      <GlobalStyles />
       <h2>Theme toggler</h2>
-    </div>
+    </ThemeProvider>
   )
 }
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+
+// This are just global styles for the entire app
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif
+  }
+
+  *, 
+  *::after, 
+  *::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    width: 100%;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: ${props => props.theme.backgroundApp};
+    color: ${props => props.theme.foreground};
+  }
+`
